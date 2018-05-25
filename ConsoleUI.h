33e0acc5dc9ac1a0301ab533f928bf2adace77a1
9ii <30561671+9ii@users.noreply.github.com>
2018-05-26 07:53:43 +0800

    !

---
+++ ConsoleUI.h
@@ -0,0 +1,20 @@
+
+
+void UIInit() {
+	system("@echo off&title Dollar C&mode con cols=73 lines=15&color 8f");
+}
+
+void Start() {
+	printf_s("\n"
+		     "\n"
+		     "                              Dollar C                                  \n"
+			 "\n"
+		     "\n"
+			 "\n"
+		     "\n"
+		     "               C                                                        \n"
+			 "   Control:   B+A                  Press any key to start               \n"
+	   		 "               D                                                        \n"
+	);
+	system("pause>null");
+}
--- DollarC.c
+++ DollarC.c
@@ -1,7 +1,10 @@
 #include <GtCui.h>
-
+#include <Windows.h>
+#include "ConsoleUI.h"
 
 int main() {
+	UIInit();
+	Start();
 	GLFWwindow	*DCMain;
 
 }
