eb00eb929683c3142305db4c1913cc6e0e13bf87
9ii <30561671+9ii@users.noreply.github.com>
2018-05-26 10:24:42 +0800

    !

--- DollarC.c
+++ DollarC.c
@@ -3,6 +3,22 @@
 #include "ConsoleUI.h"
 #include "DCEngine.h"
 
+
+
+int processInput(GLFWwindow *window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GLFW_TRUE);
+	if (glfwGetKey(window, GLFW_KEY_C) == GLFW_PRESS)
+		return 1;
+	if (glfwGetKey(window, GLFW_KEY_B) == GLFW_PRESS)
+		return 2;
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		return 3;
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		return 4;
+}
+
 int main() {
 	UIInit();
 	Start();
@@ -14,18 +30,32 @@ int main() {
 	Gt_ScrSize(600, 600);
 	glViewport(0, 0, 600, 600);
 
-
+	int DCMap_H = 1024, DCMap_W = 1024;
+	static GtRect	DC[20 * 20 + 1];
+	static _Bool *DCData;	
+	register int DCKey;
+	DCData = (_Bool)malloc(DCMap_H * DCMap_W * sizeof(_Bool));
+	int PosX=rand()*DCMap_W/16384, PosY= rand()*DCMap_H / 16384;
+	int Dx=0, Dy=0;
+	while (Dy < 20) {
+		while (Dx < 21) {
+			Gt_SetPos(DC[Dy * 20 + Dx], -1.0 + 0.1*Dx, -1.0 + 0.1*Dy, 0.095, 0.095);
+			Gt_SetColor(DC[Dy * 20 + Dx], 0.080, 0.080, 0.080, 0.5);
+		}
+	}
+	Gt_SetColor(DC[400], 0.87, 0.51, 0.15, 0.9);
 	while (!glfwWindowShouldClose(DCMain))
 	{
-		//processInput(DCMain);
-
+		DCKey = processInput(DCMain);
+		Gt_SetPos(DC[400], 0.0, 0.0, 0.06, 0.06);
 		glClearColor(0.075, 0.075,0.075, 1);
 		glClear(GL_COLOR_BUFFER_BIT);
-
+		Gt_Draw(DC, 400);
 		glfwSwapBuffers(DCMain);
 		glfwSwapInterval(1);
 		glfwPollEvents();
 	}
 	glfwTerminate();
+	free(DCData);
 	return 0;
 }
